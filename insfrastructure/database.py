151d97f317a5f7044506c8f8dfa7a1e8f451cf85
wenzskateordie@gmail.com <wenzskateordie@gmail.com>
2026-04-29 01:29:34 -0400

    conexion con la base de datos

---
+++ insfrastructure/database.py
@@ -0,0 +1,53 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from insfrastructure.config_loader import config
+from contextlib import contextmanager
+
+
+# Clase base para los modelos
+class Base(DeclarativeBase):
+    pass
+
+
+#
+def build_url() -> str:
+    db_type = config.get("db_type")
+
+    if db_type == "sqlite":
+        path = config.get("sqlite_path")
+        return f"sqlite:///{path}"
+
+    db = config.get("external_db")
+    return f"{db['engine']}://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['name']}"
+
+
+#
+def init_db() -> sessionmaker:
+    url = build_url()
+    engine = create_engine(url, echo=False)
+    Base.metadata.create_all(engine)
+    return sessionmaker(bind=engine)
+
+
+#
+_SessionFactory = None
+
+
+#
+def setup_database():
+    global _SessionFactory
+    _SessionFactory = init_db()
+
+
+#
+@contextmanager
+def get_session():
+    session = _SessionFactory()
+    try:
+        yield session
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        raise e
+    finally:
+        session.close()
